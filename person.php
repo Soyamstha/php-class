2ba936c71061303ea97f452ed131592e14284674
Soyamstha <shrestha.soyam111@gmail.com>
2024-07-28 10:21:50 +0545

    first commit

--- person.php
+++ person.php
@@ -4,7 +4,16 @@ class person
     public $name="Soyam";
     public $email="shrestha.soyam111@gmail.com";
     public $password="soaym123";
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
 }
-$student= new person;
-echo $student->name;
+
+$member1=new person("soyam");
+$member2=new person("soyam1");
+$member3=new person("soyam2");
+echo $member1->name;
+echo $member2->name;
+echo $member3->name;
 ?>
